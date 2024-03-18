4d2d07b765bb7a616cabf638194212a4ef89fd4f
Meer Uzair <meer.uxair007@gmail.com>
2024-03-18 22:05:49 +0530

    Update MobileSidebar.tsx

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -8,11 +8,14 @@ export const MobileSidebar = () => {
     <ul>
       {links.map((link) => {
         return (
-          <li className="text-gray-700 hover:text-indigo-600" key={link.title}>
+          <li className="" key={link.title}>
             <Link href={link.href}>{link.title}</Link>
           </li>
         );
       })}
+      <li>
+        <Link href="/admin-dashboard">Admin Dashboard</Link>
+      </li>
       <SignedOut>
         <li className="text-gray-700 hover:text-indigo-600">
           <Link href="/sign-in">Admin Login</Link>
